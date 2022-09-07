15825892d060b7d97a86a4727c9ecf6ee9b9c087
David <djmiller1008@yahoo.com>
2022-09-07 23:20:23 +0800

    Add edit url

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path("", views.index, name="index"),
     path("wiki/<str:entry_name>", views.entry, name="entry"),
     path("search_results", views.search_results, name="search_results"),
-    path("new", views.new, name="new")
+    path("new", views.new, name="new"),
+    path("edit/<str:entry_title>", views.edit, name="edit")
 ]
